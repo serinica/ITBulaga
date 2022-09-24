4a508e7a097a6174f5d5637c03d88a156df87b2f
serinica <110968164+serinica@users.noreply.github.com>
2022-09-24 19:42:41 +0800

    Input GUI Update

--- mapquest_parse-json_7.py
+++ mapquest_parse-json_7.py
@@ -1,69 +1,99 @@
+from tkinter import *
 import urllib.parse
-
 import requests
 
- 
+root = Tk()
 
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
 
 key = "sBTGRt94bHSZB37vjW7bKvZFLSGaVHSd"
 
+def getResult():
+    while True:
 
-while True:
+        orig = locvalue.get()
 
-    orig = input("Starting Location: ")
+        dest = destvalue.get()
 
-    if orig == "quit" or orig == "q":
-         break
 
-    dest = input("Destination: ")
+        url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
 
-    if dest == "quit" or dest == "q":
+        json_data = requests.get(url).json()
 
+        print("URL: " + (url))
+
+        json_data = requests.get(url).json()
+
+        json_status = json_data["info"]["statuscode"]
+
+        if json_status == 0:
+
+            print("API Status: " + str(json_status) + " = A successful route call.\n")
+
+            print("=============================================")
+            print("Directions from " + (orig) + " to " + (dest))
+
+            print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
+
+            print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
+
+            print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
+
+            print("=============================================")
+
+            for each in json_data["route"]["legs"][0]["maneuvers"]:
+
+                print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
+            print("=============================================\n")
+
+        elif json_status == 402:
+
+            print("**********************************************")
+
+            print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
+
+            print("**********************************************\n")
+
+        elif json_status == 611:
+
+            print("**********************************************")
+
+            print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
         break
 
-    url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
+def Close():
+    root.destroy()
 
-    json_data = requests.get(url).json()
 
-    print("URL: " + (url))
+root.geometry("644x344")
+#Heading
+Label(root, text="MAPQUEST API", font="comicsansms 13 bold", pady=15).grid(row=0, column=3)
 
-    json_data = requests.get(url).json()
+#Text for our form
+loc = Label(root, text="Starting Location")
+dest = Label(root, text="Destination")
 
-    json_status = json_data["info"]["statuscode"]
+#Pack text for our form
+loc.grid(row=1, column=2)
+dest.grid(row=2, column=2)
 
-    if json_status == 0:
+# Tkinter variable for storing entries
+locvalue = StringVar()
+destvalue = StringVar()
 
-        print("API Status: " + str(json_status) + " = A successful route call.\n")
 
-        print("=============================================")
+#Entries for our form
+locentry = Entry(root, textvariable=locvalue)
+destentry = Entry(root, textvariable=destvalue)
 
-        print("Directions from " + (orig) + " to " + (dest))
+# Packing the Entries
+locentry.grid(row=1, column=3)
+destentry.grid(row=2, column=3)
 
-        print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
 
-        print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
+#Button & packing it and assigning it a command
+Button(text="Submit", command=getResult).grid(row=7, column=3)
 
-        print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
+Button(root, text="Exit", command=Close).grid(row=8, column=3)
 
-        print("=============================================")
-
-        for each in json_data["route"]["legs"][0]["maneuvers"]:
-
-            print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
-
-        print("=============================================\n")
-
-    elif json_status == 402:
-
-        print("**********************************************")
-
-        print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
-
-        print("**********************************************\n")
-
-    elif json_status == 611:
-
-        print("**********************************************")
-
-        print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
+root.mainloop()
