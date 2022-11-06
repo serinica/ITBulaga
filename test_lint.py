ff1bf7fcbdedb599f4180152baf92c36955890eb
serinica <110968164+serinica@users.noreply.github.com>
2022-11-06 16:24:56 +0800

    Update test_lint.py

--- test_lint.py
+++ test_lint.py
@@ -8,22 +8,15 @@ from pylint.lint import Run
 
 THRESHOLD = 1
 
-#run = lint.Run(["mapquest_parse-json_7.py"], do_exit=False) 
-
-#score = run
-
 default_stdout = sys.stdout
 sys.stdout = type("Dummy", (TextIO,), {"write": lambda self, data: ()})()
 score1 = Run(["mapquest_parse-json_7.py"], exit=False).linter.stats.global_note
 sys.stdout = default_stdout
 
-print("Code Rating: ", score1, "/4")
-
 if score1 < THRESHOLD: 
-
     print("Linter failed: Score < threshold value") 
-
     sys.exit(1) 
-
+else:
+    print("Code Rating Based on Lint Testing:", score1,"/ 5 \n (((PASSED)))")
 
 sys.exit(0) 
